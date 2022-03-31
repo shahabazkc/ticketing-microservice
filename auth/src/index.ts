16c2b465776e6353a175b2f83c7efe4f42e93064
Shahabaz Kc <55649768+shahabazkc@users.noreply.github.com>
2022-03-31 15:07:58 +0530

    Update: Error handler added; mongo-deb added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,13 @@
+"use strict";
 import express from 'express';
+import 'express-async-errors';
+import { NotFoundError } from './errors/not-found-error';
+import { errorHandler } from './middlewares/error-handler';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -14,6 +19,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
+app.all('*', () => {
+    throw new NotFoundError()
+});
+
+app.use(errorHandler);
+
 app.listen(3000, () => {
     console.log("Listening on port 3000!!!!");
-})
+});
